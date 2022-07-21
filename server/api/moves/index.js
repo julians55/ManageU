ab89b7e58f01eeaa7ad03e1339eb98555ce45415
Julian Salamanca <jsalamancah@unal.edu.co>
2022-07-20 22:48:01 -0500

    fix/ correct balance bug

--- server/api/moves/index.js
+++ server/api/moves/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const {
     getMoves,
+    getLastMoves,
     getMoveById,
     createMove,
     getMovesByUser,
@@ -11,6 +12,7 @@ const {isAuth} = require('../../auth/auth.service');
 const router = Router();
 
 router.get('/',isAuth(), getMoves);
+router.get('/last',isAuth(), getLastMoves);
 router.get('/:id', getMoveById);
 router.get('/user/:id', getMovesByUser);
 router.post('/',isAuth(), createMove);
--- server/api/moves/moves.controller.js
+++ server/api/moves/moves.controller.js
@@ -1,11 +1,14 @@
 const pool = require('../../config/database');
 
 const getMoves = async (req, res) => {
-    const response = await pool.query('SELECT * FROM public."moves"');
-    console.log(response.rows);
+    const response = await pool.query('SELECT * FROM public."moves" WHERE "ID" = $1', [req.body.user]);
     res.send(response.rows);
 }
 
+const getLastMoves = async (req, res) => {
+    const response = await pool.query('SELECT *FROM public."moves" order by "ID" desc limit 10');
+    res.send(response.rows)
+}
 const getMoveById = async (req, res) => {
     const move = await pool.query('SELECT * FROM public."moves" WHERE "ID" = $1', [req.params.id]);
     res.send(move.rows);
@@ -19,19 +22,24 @@ const getMovesByUser = async (req, res) => {
 
 const createMove = async (req, res)=>{
     const { type, category, amount, concept, date} = req.body;
+    console.log(req.body.user);
     if (!type ) {
       res.status(401).json({message: "Bad request"});
     }
-    console.log(req.body);
     const newMove = await pool.query(
         'INSERT INTO public."moves" ("type", "category", "amount", "concept", "date", "user") VALUES ($1,$2,$3,$4,$5,$6) RETURNING *',
         [req.body.type,req.body.category,req.body.amount,req.body.concept, req.body.date, req.body.user]
     );
     const user = await pool.query('SELECT * FROM public."user" WHERE "ID" = $1', [req.body.user]);
-    const balance = parseFloat(user.rows[0].balance.slice(1));
-
+    let balance;
+    if(user.rows[0].balance[0]=='-'){
+        balance = parseFloat(user.rows[0].balance.slice(2))*-1;
+    }else{
+        balance = parseFloat(user.rows[0].balance.slice(1));
+    }
+    console.log(balance);
     const newBalance = req.body.type=="income"? balance + req.body.amount: balance - req.body.amount;
-    
+    console.log(newBalance);
     await pool.query(
         'UPDATE public."user" SET "balance" = $1 WHERE "ID" = $2',
         [newBalance, req.body.user]
@@ -67,6 +75,7 @@ const editMove = async (req, res) => {
 
 module.exports = {
     getMoves,
+    getLastMoves,
     getMoveById,
     createMove,
     getMovesByUser,
